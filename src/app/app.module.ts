c5267cf4cf8145be9e7f1a19839ba288e2fa65cb
CarlosX26 <cjunior1@live.com>
2024-09-18 17:03:26 -0300

    feat: auth config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from './app.component';
 import { FormLoginComponent } from './components/form-login/form-login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { OAuthModule } from 'angular-oauth2-oidc';
+import { AuthConfig, OAuthModule, OAuthService } from 'angular-oauth2-oidc';
 import { ProtectedComponent } from './pages/protected/protected.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+
+export const authConfig: AuthConfig = {
+  issuer: 'http://localhost:8080/realms/my-realm',
+  redirectUri: window.location.origin,
+  clientId: 'app_angular',
+  responseType: 'code', // Usar o fluxo Authorization Code
+  scope: 'openid profile email', // Scopes para obter o perfil do usuário
+  showDebugInformation: true,
+  strictDiscoveryDocumentValidation: false,
+};
 
 @NgModule({
   declarations: [
@@ -28,4 +38,13 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private oauthService: OAuthService) {
+    this.configure();
+  }
+
+  private configure() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+}
